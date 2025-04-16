2b64e811a2a81f9ca3485a9b60fd5b585cf90cad
ponzS <148766667+ponzS@users.noreply.github.com>
2025-04-17 02:19:14 +0800

    Delete src directory

--- src/components/phone/ContactsS.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <FriendList />
-</template>
--- src/pages/creatkeypage.vue
+++
@@ -1,8 +0,0 @@
-<!-- eslint-disable vue/multi-word-component-names -->
-<template>
-  <ion-page>
-    <ion-content>
-      <CreatKey />
-    </ion-content>
-  </ion-page>
-</template>
